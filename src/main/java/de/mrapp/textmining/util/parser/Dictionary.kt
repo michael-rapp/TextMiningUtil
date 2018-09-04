b5c0d961147ed9bb2f49d9cc9acd913dfdd29947
michael-rapp <michael.rapp90@googlemail.com>
2018-09-04 12:10:42 +0200

    Add class Dictionary.

---
+++ src/main/java/de/mrapp/textmining/util/parser/Dictionary.kt
@@ -0,0 +1,69 @@
+/*
+ * Copyright 2017 - 2018 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.textmining.util.parser
+
+import java.io.Serializable
+
+/**
+ * A dictionary that contains several entries, which can be used to identify known values and
+ * assign them to predefined translations.
+ *
+ * @param K The type of the values to be translated
+ * @param V The type of the translations
+ * @author Michael Rapp
+ * @since 2.1.0
+ */
+class Dictionary<K, V> : Iterable<Dictionary.Entry<K, V>>, Serializable {
+
+    /**
+     * An entry of a [Dictionary].
+     *
+     * @property key             The known value to be translated
+     * @property value           The translation
+     * @property associationType An optional association type
+     */
+    data class Entry<K, V> @JvmOverloads constructor(
+            val key: K, val value: V, val associationType: AssociationType? = null) :
+            Serializable
+
+    private val entries: MutableMap<K, Entry<K, V>> = HashMap()
+
+    /**
+     * Adds a new [entry] to the dictionary.
+     */
+    fun addEntry(entry: Entry<K, V>) {
+        this.entries[entry.key] = entry
+    }
+
+    /**
+     * Returns the entry that corresponds to a specific [key].
+     */
+    fun lookup(key: K) = entries[key]
+
+    /**
+     * Returns [Matches] for all entries that match a certain [value] according to a given
+     * [matcher].
+     */
+    fun <T> lookup(value: T, matcher: Matcher<T, K>): Matches<T, Entry<K, V>> {
+        val matches = entries.values.map { entry ->
+            matcher.getMatch(value, entry.key)?.let { match ->
+                Match(value, entry, match.heuristicValue)
+            }
+        }.filter { it != null }.requireNoNulls()
+        return Matches(matches, matcher.isGainMetric())
+    }
+
+    override fun iterator() = entries.values.iterator()
+
+}
---
+++ src/test/java/de/mrapp/textmining/util/parser/DictionaryTest.kt
@@ -0,0 +1,74 @@
+/*
+ * Copyright 2017 - 2018 Michael Rapp
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+package de.mrapp.textmining.util.parser
+
+import de.mrapp.textmining.util.metrics.LevenshteinDistance
+import kotlin.test.*
+
+/**
+ * Tests the functionality of the class [Dictionary].
+ */
+class DictionaryTest {
+
+    @Test
+    fun testLookupByKey() {
+        val dictionary = Dictionary<String, Int>()
+        dictionary.addEntry(Dictionary.Entry("one", 1))
+        dictionary.addEntry(Dictionary.Entry("two", 2, AssociationType.BIDIRECTIONAL))
+        dictionary.addEntry(Dictionary.Entry("three", 3))
+        val entry = dictionary.lookup("two")
+        assertNotNull(entry)
+        assertEquals("two", entry!!.key)
+        assertEquals(2, entry.value)
+        assertEquals(AssociationType.BIDIRECTIONAL, entry.associationType)
+    }
+
+    @Test
+    fun testLookupByKeyReturnsNull() {
+        val dictionary = Dictionary<String, Int>()
+        dictionary.addEntry(Dictionary.Entry("one", 1))
+        dictionary.addEntry(Dictionary.Entry("two", 2))
+        dictionary.addEntry(Dictionary.Entry("three", 3))
+        val entry = dictionary.lookup("foo")
+        assertNull(entry)
+    }
+
+    @Test
+    fun testLookupUsingMatcher() {
+        val dictionary = Dictionary<String, Int>()
+        dictionary.addEntry(Dictionary.Entry("one", 1))
+        dictionary.addEntry(Dictionary.Entry("two", 2))
+        dictionary.addEntry(Dictionary.Entry("twoo", 2))
+        dictionary.addEntry(Dictionary.Entry("three", 3))
+        val matches = dictionary.lookup("two", Matcher.metric(LevenshteinDistance(), 1.0))
+        assertNotNull(matches)
+        assertTrue(matches.iterator().asSequence()
+                .contains(Match("two", Dictionary.Entry("two", 2), 0.0)))
+        assertTrue(matches.iterator().asSequence()
+                .contains(Match("two", Dictionary.Entry("twoo", 2), 1.0)))
+    }
+
+    @Test
+    fun testLookupUsingMatcherReturnsNoMatches() {
+        val dictionary = Dictionary<String, Int>()
+        dictionary.addEntry(Dictionary.Entry("one", 1))
+        dictionary.addEntry(Dictionary.Entry("two", 2))
+        dictionary.addEntry(Dictionary.Entry("twoo", 2))
+        dictionary.addEntry(Dictionary.Entry("three", 3))
+        val matches = dictionary.lookup("foo", Matcher.metric(LevenshteinDistance(), 1.0))
+        assertNotNull(matches)
+        assertFalse(matches.iterator().hasNext())
+    }
+
+}
