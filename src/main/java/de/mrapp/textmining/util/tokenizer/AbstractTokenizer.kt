40f609765ba601b72dee479cd52ed867a576ec83
Michael Rapp <michael.rapp90@googlemail.com>
2019-01-21 21:49:05 +0100

    Format code.

--- src/main/java/de/mrapp/textmining/util/tokenizer/AbstractTokenizer.kt
+++ src/main/java/de/mrapp/textmining/util/tokenizer/AbstractTokenizer.kt
@@ -26,14 +26,13 @@ abstract class AbstractTokenizer<TokenType : Token> : Tokenizer<TokenType> {
 
     /**
      * Adds a new [token] to a [map], if it is not already contained. Otherwise, the token's
-     * [position] is added to the existing token. A [tokenFactory] for converting the given
-     * [token] to the type [T] must be provided.
+     * [position] is added to the existing token. A [tokenFactory] for converting the given [token]
+     * to the type [T] must be provided.
      *
      * @param T The type of the token
      */
-    fun <T : Token> addToken(map: MutableMap<String, T>,
-                                                      token: String, position: Int,
-                                                      tokenFactory: (String, Int) -> T) {
+    fun <T : Token> addToken(map: MutableMap<String, T>, token: String, position: Int,
+                             tokenFactory: (String, Int) -> T) {
         var existingToken = map[token]
 
         if (existingToken == null) {
@@ -48,8 +47,7 @@ abstract class AbstractTokenizer<TokenType : Token> : Tokenizer<TokenType> {
      * The method, which is invoked on subclasses in order to tokenize a specific [text]. The tokens
      * should be added to the given [map].
      */
-    protected abstract fun onTokenize(text: String,
-                                      map: MutableMap<String, TokenType>)
+    protected abstract fun onTokenize(text: String, map: MutableMap<String, TokenType>)
 
     override fun tokenize(text: CharSequence): Collection<TokenType> {
         if (text.isNotEmpty()) {
